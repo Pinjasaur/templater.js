7af3932cea67c551ad6f7e940b1a891767b276db
Paul Esch-Laurent <herppfel@gmail.com>
2016-06-23 19:08:54 -0400

    Make more production-ready
    Wrap in IIFE, tweak regex (0 or 1 space between double braces), attach
    to global object in IIFE, change #if to check for falsy value (instead
    of using obj.hasOwnProperty(prop))

--- dist/templater.js
+++ dist/templater.js
@@ -2,73 +2,85 @@
  * templater.js v1.0.0
  * @author Paul Esch-Laurent | @license MIT
  */
-function templater(template) {
+(function(global) {
+
   "use strict";
-  
-  // return a function to use the replacement values
-  return function(data) {
+
+  if (global.templater) {
+    throw new Error("templater already defined.");
+  }
+
+  function templater(template) {
     
-    // regex for the #if, body, /if
-    var start = "{{\\s*#if\\s+\\((.+)\\)\\s*}}",
-        end = "{{\\s*/if\\s*}}",
-        regex = start + "[\\s\\S]+?" + end,
-        // save a reference to the template so it can be restored at the end
-        tmpl = template,
-        match, conditions, meetsConditions;
-    
-    // keep matching conditions until there are none
-    while (match = new RegExp(regex, "ig").exec(template)) {
+    // return a function to use the replacement values
+    return function(data) {
       
-      // assume it meets all conditions
-      meetsConditions = true;
-      // remove all whitespace between conditions, split on commas
-      conditions = match[1].replace(/\s+/g, "").split(",");
+      // regex for the #if, body, /if
+      var start = "{{\\s?#if\\s+\\((.+)\\)\\s?}}",
+          end = "{{\\s?/if\\s?}}",
+          regex = start + "[\\s\\S]+?" + end,
+          // save a reference to the template so it can be restored at the end
+          tmpl = template,
+          match, conditions, meetsConditions;
       
-      // check each condition to see if it exists in the data
-      for (var i = 0; i < conditions.length; i++) {
-        // if it doesn't, break out
-        if (!data.hasOwnProperty(conditions[i])) {
-          meetsConditions = false;
-          break;
+      // keep matching conditions until there are none
+      while (match = new RegExp(regex, "ig").exec(template)) {
+        
+        // assume it meets all conditions
+        meetsConditions = true;
+        // remove all whitespace between conditions, split on commas
+        conditions = match[1].replace(/\s+/g, "").split(",");
+        
+        // check each condition to see if it exists in the data
+        for (var i = 0; i < conditions.length; i++) {
+          // if it doesn't, break out
+          if (!data[conditions[i]]) {
+            meetsConditions = false;
+            break;
+          }
+        }
+        
+        // check if it meets the conditions
+        if (!meetsConditions) {
+          // wipe the #if, /if, and body if it doesn't
+          template = template.split(match[0]).join("");
+        } else {
+          // otherwise just get rid of the #if and /if statements
+          var open = new RegExp(start, "ig"), close = new RegExp(end, "ig");
+          // beginning of the template
+          template = template.substring(0, match["index"])
+              // body of the condition
+            + match[0].replace(open, "").replace(close, "")
+              // end of the template
+            + template.substring(match["index"] + match[0].length, template.length);
         }
       }
       
-      // check if it meets the conditions
-      if (!meetsConditions) {
-        // wipe the #if, /if, and body if it doesn't
-        template = template.split(match[0]).join("");
-      } else {
-        // otherwise just get rid of the #if and /if statements
-        var open = new RegExp(start, "i"), close = new RegExp(end, "i");
-        // beginning of the template
-        template = template.substring(0, match["index"])
-            // body of the condition
-          + match[0].replace(open, "").replace(close, "")
-            // end of the template
-          + template.substring(match["index"] + match[0].length, template.length);
-      }
-    }
-    
-    // loop through each property of the data
-    for (var prop in data) {
-      // create the regex to match the '{{ prop }}' format
-      regex = "{{\\s*" + prop + "\\s*}}";
-      // replace the instances in the template with the property value (escaping &, <, and > if necessary)
-      template = template.replace(new RegExp(regex, "ig"), ("" + data[prop]).replace(/[&<>]/g, function(tag) {
-        // replace &, <, or > if necessary
-        var replacements = {
-          "&": "&amp;",
-          "<": "&lt;",
-          ">": "&gt;"
-        };
+      // loop through each property of the data
+      for (var prop in data) {
+        // create the regex to match the '{{ prop }}' format
+        regex = "{{\\s*" + prop + "\\s*}}";
+        // replace the instances in the template with the property value (escaping &, <, and > if necessary)
+        template = template.replace(new RegExp(regex, "ig"), ("" + data[prop]).replace(/[&<>]/g, function(tag) {
+          // replace &, <, or > if necessary
+          var replacements = {
+            "&": "&amp;",
+            "<": "&lt;",
+            ">": "&gt;"
+          };
 
-        return replacements[tag] || tag;
-      }));
+          return replacements[tag] || tag;
+        }));
+      }
+      
+      // set the template back to its original state (so it can be used again)
+      var ret = template;
+      template = tmpl;
+      return ret;
     }
-    
-    // set the template back to its original state (so it can be used again)
-    var ret = template;
-    template = tmpl;
-    return ret;
   }
-}
+
+  // expose templater globally
+  global.templater = templater;
+
+})(typeof window === "undefined" ? this : window);
